afb5ce1674980b203fc5d08f1f24e8fe0d3af78b
Denysko8 <151399543+Denysko8@users.noreply.github.com>
2024-03-14 22:20:25 +0200

    Perfect main.py
    made it work

--- main.py
+++ main.py
@@ -1,23 +1,82 @@
-def calculate_work_duration(k, t, arr):
+def check_forward(arr, k):
+    final_list_forward = []
+    mean = sum(arr) / len(arr)
 
-    work_duration = [0] * k
+    max_el = max(arr)
+    if arr[-2] == max_el:
+        max_el = max_el + arr[-1]
+    elif arr[1] == max_el:
+        max_el = max_el + arr[0]
 
-    if k < len(arr):
-        board_per_painter = len(arr) // k
-        remaining_boards = len(arr) % k
-        start_index = 0
-        for item in range(k):
-            end_index = start_index + board_per_painter
-            if remaining_boards > 0:
-                end_index += 1
-                remaining_boards -= 1
+    middle = (sum(arr) + (max_el + mean) / 2) / 2
 
-            work_duration[item] = sum(arr[start_index:end_index]) * t
-            start_index = end_index
-    else:
+    while middle >= max_el:
+
+        lst_forward = []
+        result = 0
+        painters = k
+
         for i in range(len(arr)):
-            work_duration[i] = arr[i] * t
+            if painters == 1:
+                result += sum(arr[i:])
+                lst_forward.append(result)
+                break
+            if result > middle:
+                break
+            result += arr[i]
+            if result > middle or i == len(arr) - 1:
+                if i == len(arr) - 1:
+                    lst_forward.append(result)
+                else:
+                    lst_forward.append(result - arr[i])
+                painters -= 1
+                result = arr[i]
 
-    result = max(work_duration)
+        final_list_forward.append(max(lst_forward))
+        middle = int((middle + (max_el + mean) / 2) / 2)
+    return final_list_forward
 
-    return result
+
+def check_backwards(arr, k):
+    final_list_backward = []
+    mean = sum(arr) / len(arr)
+
+    max_el = max(arr)
+    if arr[-2] == max_el:
+        max_el = max_el + arr[-1]
+    elif arr[1] == max_el:
+        max_el = max_el + arr[0]
+
+    middle = (sum(arr) + (max_el + mean) / 2) / 2
+
+    while middle >= max_el:
+        lst_backward = []
+        result = 0
+        painters = k
+
+        for i in range(len(arr) - 1, -1, -1):
+            if painters == 1:
+                result += sum(arr[:i]) + arr[i]
+                lst_backward.append(result)
+                break
+            result += arr[i]
+            if result > middle or i == 0:
+                if i == 0:
+                    lst_backward.append(result)
+                else:
+                    lst_backward.append(result - arr[i])
+                painters -= 1
+                result = arr[i]
+
+        final_list_backward.append(max(lst_backward))
+        middle = int((middle + (max_el + mean) / 2) / 2)
+    return final_list_backward
+
+
+def final_result(painters, time, array):
+    returned_list_forward = check_forward(array, painters)
+    returned_list_backward = check_backwards(array, painters)
+    gathered_list = returned_list_forward + returned_list_backward
+    answer = min(gathered_list) * time
+    return answer
+
