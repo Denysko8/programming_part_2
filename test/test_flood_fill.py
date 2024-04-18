cff36c8ee7073664290e0388d268c901d0e51b67
Denysko8 <151399543+Denysko8@users.noreply.github.com>
2024-04-19 02:10:13 +0300

    Renames txt files along with their usages in main file

---
+++ test/test_flood_fill.py
@@ -0,0 +1,36 @@
+import unittest
+from flood_fill import flood_fill, read_input_file, write_output_file
+
+
+class TestFloodFill(unittest.TestCase):
+    def test_flood_fill(self):
+        input_filename = 'input_test.txt'
+        with open(input_filename, 'w') as file:
+            file.write('4,4\n')
+            file.write('2,2\n')
+            file.write('X\n')
+            file.write('O, X, X, O\n')
+            file.write('X, X, O, X\n')
+            file.write('X, X, O, O\n')
+            file.write('O, O, O, O\n')
+
+        output_filename = 'output_test.txt'
+
+        height, width, start, new_color, matrix = read_input_file(input_filename)
+        result_matrix = flood_fill(matrix, start, matrix[start[0]][start[1]], new_color)
+        write_output_file(output_filename, result_matrix)
+
+        expected_result = [
+            ['O', 'X', 'X', 'O'],
+            ['X', 'X', 'O', 'X'],
+            ['X', 'X', 'O', 'O'],
+            ['O', 'O', 'O', 'O']
+        ]
+        with open(output_filename, 'r') as file:
+            actual_result = [list(map(str.strip, line.strip().split(', '))) for line in file]
+
+        self.assertEqual(actual_result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
