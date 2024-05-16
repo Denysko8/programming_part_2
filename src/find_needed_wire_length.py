ff09a950d5d04aa0704bddca37d737df16b1e56e
Denysko8 <denisnesterovic2@gmail.com>
2024-05-16 23:04:22 +0300

    updated main (still not perfect)

--- src/find_needed_wire_length.py
+++ src/find_needed_wire_length.py
@@ -1,18 +1,14 @@
-from math import sqrt
+import math
 
+def max_wire_length(w, max_heights):
+    n = len(max_heights)
+    max_diff = max(max_height - 1 for max_height in max_heights)
+    max_length = (n - 1) * math.hypot(w, max_diff)
+    return round(max_length, 2)
 
-def find_needed_wire_length(distance_between_pillars, pillars):
-    needed_wire_length = 0
+print(max_wire_length(2, [3, 3, 3]))  # Output: 5.66
+print(max_wire_length(100, [1, 1, 1, 1]))  # Output: 300.0
+print(max_wire_length(4, [100, 2, 100, 2, 100]))  # Output: 396.32
 
-    if 0 >= distance_between_pillars < 100:
-        return None
-
-    for i in range(1, len(pillars)):
-        if any(0 >= pillar < 100 for pillar in pillars):
-            return None
-        distance_between_pillars_in_height = pillars[i] - pillars[i - 1]
-        wire_between_pillars = sqrt(
-            distance_between_pillars_in_height**2 + distance_between_pillars**2
-        )
-        needed_wire_length += wire_between_pillars
-    return round(needed_wire_length, 2)
+heights = [56, 18, 17, 94, 23, 7, 21, 94, 29, 54, 44, 26, 86, 79, 4, 15, 5, 91, 25, 17, 88, 66, 28, 2, 95, 97, 60, 93, 40, 70, 75, 48, 38, 51, 34, 52, 87, 8, 62, 77, 35, 52, 3, 93, 34, 57, 51, 11, 39, 72]
+print(max_wire_length(4, heights))  # Output: 2738.18
